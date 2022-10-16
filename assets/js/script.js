15c216aa6676cd28da71c2fd18acfccf784be083
ahsanaaxioned <ahsana@axioned.com>
2022-10-16 14:11:13 +0530

    js correction done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,64 @@
 // global variable declaration start here
-let a = 0;
-let item = document.querySelectorAll(".slide-item");
-let left = document.querySelector(".left");
-let right = document.querySelector(".right");
-let btn = document.querySelectorAll(".btn");
-let topp = document.querySelector(".top");
+const listItem = document.querySelectorAll(".slide-item"),
+    left = document.querySelector(".left"),
+    right = document.querySelector(".right"),
+    btn = document.querySelectorAll(".btn"),
+    topp = document.querySelector(".top");
+let index = 0;
 // global variable declaration end here
 
 // slider function start here
-function slideShow(a) {
-    let togg = document.querySelector(".active");
-    togg.classList.remove("active");
-    item[a].classList.add("active");
-    let btnActive = document.querySelector(".btn-active");
+function slideShow(index) {
+    const toggle = document.querySelector(".active");
+    toggle.classList.remove("active");
+    listItem[index].classList.add("active");
+    const btnActive = document.querySelector(".btn-active");
     btnActive.classList.remove("btn-active");
-    btn[a].classList.add("btn-active");
-
+    btn[index].classList.add("btn-active");
 };
 // slider function end here
 
 // event for slider start here
 left.addEventListener("click", function () {
-    a--;
-    if (a < 0) {
-        a = item.length - 1;
+    index--;
+    if (index < 0) {
+        index = listItem.length - 1;
     };
-    slideShow(a);
+    slideShow(index);
 });
 right.addEventListener("click", function () {
-    a++;
-    if (a > item.length - 1) {
-        a = 0;
+    index++;
+    if (index > listItem.length - 1) {
+        index = 0;
     };
-    slideShow(a);
+    slideShow(index);
 });
 // event for slider end here
 
 // slider button event start here
+function addClass() {
+    for (let li of listItem) {
+        li.classList.remove("active");
+    };
+}
 btn.forEach(function (ele, i) {
     ele.addEventListener("click", function () {
-        let btnActive = document.querySelector(".btn-active");
+        const btnActive = document.querySelector(".btn-active");
         btnActive.classList.remove("btn-active");
         btn[i].classList.add("btn-active");
-        for (let li of item) {
-            li.classList.remove("active");
-        };
-        item[i].classList.add("active");
-        a=i;
+        index = i;
+        addClass();
+        listItem[i].classList.add("active");
     });
 });
 // slider button event end here
 
 // window scroll event start here
-topp.addEventListener("click",function(){
+topp.addEventListener("click", function () {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
-      });
+    });
 });
 // window scroll event end here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
